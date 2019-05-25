72dee4ee39d9404774be781dee76e362c777891c
Josh Brown <brown.joshua49@aol.com>
2019-05-25 13:17:02 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject twitter-graph "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [reagent "0.9.0-SNAPSHOT"]]
+  :source-paths ["src/cljs" "src/clj" "src/js"]
+  )
